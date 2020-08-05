0e741feb43e9ce73035fd7e04c157148603dc949
yanmyo45 <yanmyokyaw.kantech@gmail.com>
2020-08-05 16:34:23 +0630

    commit

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,34 @@
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+const String APP_ID = "3042";
+const String AUTH_KEY = "JbAK28Fz9A5ZFvc";
+const String AUTH_SECRET = "Db2qtgDn5ymC8Bc";
+const String ACCOUNT_ID = "wvw84zBUg623xC_xtm9R";
+const String DEFAULT_PASS = "weareone";
+
+List<CubeUser> users = [
+  CubeUser(
+    id: 1740482,
+    login: "call_user_1",
+    fullName: "User 1",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1740484,
+    login: "call_user_2",
+    fullName: "User 2",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1740488,
+    login: "call_user_3",
+    fullName: "User 3",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1740494,
+    login: "call_user_4",
+    fullName: "User 4",
+    password: DEFAULT_PASS,
+  ),
+];
